a7b3da50a1b481f89679ca3fed4fbbae0e19ea6a
unknown <varungupta1305@gmail.com>
2023-05-17 23:15:46 +0530

    first commit

---
+++ app/src/main/java/com/example/musicify/AudioModel.java
@@ -0,0 +1,27 @@
+package com.example.musicify;
+
+import java.io.Serializable;
+
+public class AudioModel implements Serializable {
+    String path;
+    String title;
+    String duration;
+
+    public AudioModel(String path,String title, String duration){
+        this.path=path;
+        this.title=title;
+        this.duration=duration;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+}
